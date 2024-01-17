eb43233ac895f1dece023c61d268d8eb9e21b016
saumitraid <saumitraid.das@gmail.com>
2024-01-17 12:07:27 +0530

    Create prac1.py

---
+++ prac1.py
@@ -0,0 +1,19 @@
+print("Hello")
+
+a=10
+b=45.89
+
+print(type(a))
+print(type(b))
+
+st="This is a simple string"
+st1='Hello students'
+
+print(type(st))
+print(type(st1))
+
+print(2**3)
+
+print(9.9//2)
+
+print("Hello"+" "+"9")
