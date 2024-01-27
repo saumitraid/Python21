c5ecf2702f97291e3784cd6de4dd69bcbb34c469
saumitraid <saumitraid.das@gmail.com>
2024-01-27 18:10:03 +0530

    27-01-2024

---
+++ class1.py
@@ -0,0 +1,11 @@
+class Vehicle:
+    def moveForward(self):
+        print("Go forward")
+
+    def moveBackward(self):
+        print("Go backward")
+
+obj=Vehicle()
+obj.moveForward()
+obj.moveBackward()
+
